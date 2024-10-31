2c8fd1b93d6d758d6d47c6825b70f9d2248bebde
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-10-31 15:02:39 -0300

    update

--- script/script.js
+++ script/script.js
@@ -1,37 +1,37 @@
 const canvas = document.getElementById("canvas"); //Canvas
 const ctx = canvas.getContext("2d"); //Contexto do canvas
 
-const containerScreen = document.querySelector(".game");
-
 const startScreen = document.querySelector(".start-screen");
-const gameScreen = document.querySelector(".game-screen");
-const gameoverScreen = document.querySelector(".gameover-screen");
+const gameoverScreen = document.querySelector(".end-screen");
 const settingsScreen = document.querySelector(".settings-screen");
 
-const btnPlayGame = document.getElementById("btnPlayGame"); //Botão que inicia o jogo
-const btnPlayAgain = document.querySelectorAll(".play-again"); //Botão que reinicia o jogo
+const btnPlayGame = document.querySelector(".play-game"); //Botão que inicia o jogo
+const btnPlayAgain = document.querySelector(".play-again"); //Botão que reinicia o jogo
 
-const btnPauseGame = document.getElementById("btnPauseGame"); //Botão para pausar o jogo
-const pauseIcon = document.querySelector(".change-pause-icon"); //Botão para pausar o jogo
-
-const btnOpenSettings = document.querySelectorAll(".open-settings"); //Botão para abrir o modal de configurações
+const btnOpenSettings = document.querySelector(".open-settings"); //Botão para abrir o modal de configurações
 const btnCloseSettings = document.querySelectorAll(".close-settings"); //Botão para fecha o modal de configurações
 
-const Dpad = document.getElementById("Dpad");
-const btnUp = document.getElementById("btnUp");
-const btnRight = document.getElementById("btnRight");
-const btnDown = document.getElementById("btnDown");
-const btnLeft = document.getElementById("btnLeft");
+const Dpad = document.querySelector(".dpad");
+const btnUp = document.querySelector(".up");
+const btnRight = document.querySelector(".right");
+const btnDown = document.querySelector(".down");
+const btnLeft = document.querySelector(".left");
 
-const buttonSpeed = document.querySelectorAll(".button-speed");
-const buttonGrid = document.querySelector(".button-grid");
-const gridIcon = document.querySelector(".grid-icon");
+const score = document.querySelector(".score");
+const finalScore = document.querySelector(".final-score");
 
 const buttonBackgroundColor = document.querySelectorAll(
   ".button-background-color"
 );
-
 const buttonSnakeColor = document.querySelectorAll(".button-snake-color");
+const buttonFoodColor = document.querySelectorAll(".button-food-color");
+
+const buttonGrid = document.querySelector(".button-grid");
+const buttonCollision = document.querySelector(".button-collision");
+
+const toggleButton = document.querySelectorAll(".toggle-button");
+
+const buttonSpeed = document.querySelectorAll(".button-speed");
 
 /*-----Configurações do canvas-----*/
 const size = 10; //Tamanho dos elementos no jogo
@@ -42,66 +42,14 @@ let canvasSize = {
   y: canvas.height,
 };
 
-// Função para ajustar o tamanho da tela de game
-const setGameScreen = () => {
-  let windowX = window.innerWidth;
-  let windowY = window.innerHeight;
-
-  if (windowX > windowY) {
-    horizontalScreen(windowX, windowY);
-    setHorizontalCanvas(windowX, windowY);
-    Dpad.style.display = "none";
-  } else {
-    verticalScreen(windowX);
-    setVerticalCanvas(windowX);
-    Dpad.style.display = "grid";
-  }
-};
-
-const horizontalScreen = (numX, numY) => {
-  containerScreen.style.width = `${numX - 100}px`;
-  containerScreen.style.height = `${numY - 100}px`;
-};
-
-const setHorizontalCanvas = (numX, numY) => {
-  let horizontalX = Math.round(numX / 10) * 10 - 300;
-  let horizontalY = Math.round(numY / 10) * 10 - 300;
-
-  canvas.width = horizontalX;
-  canvas.height = horizontalY;
-
-  canvasSize.x = horizontalX;
-  canvasSize.y = horizontalY;
-};
-
-const verticalScreen = (numX) => {
-  containerScreen.style.width = `${numX / 2 + 100}px`;
-  containerScreen.style.height = `${numX / 2 + 100}px`;
-};
-
-const setVerticalCanvas = (numX) => {
-  let horizontalX = Math.round(numX / 10) * 10;
-  let horizontalY = horizontalX - 100;
-
-  canvas.width = horizontalX;
-  canvas.height = horizontalY;
-
-  canvasSize.x = horizontalX;
-  canvasSize.y = horizontalY;
-};
-
-setGameScreen();
-
-window.addEventListener("resize", setGameScreen);
-
 const canvasLimitX = canvasSize.x - size; //Limite horizontal do canvas
 const canvasLimitY = canvasSize.y - size; //Limite vertical do canvas
 
 //Cor de fundo do jogo
 let colorGame = {
-  color1: "#1a2625",
-  color2: "#1d803b",
-  color3: "#1a2625",
+  color1: "white",
+  color2: "white",
+  color3: "white",
 };
 
 const drawBackground = () => {
@@ -119,9 +67,35 @@ const drawBackground = () => {
   ctx.fillRect(0, 0, canvasSize.x, canvasSize.y);
 };
 
-let gridON = true; //Liga e desliga o grid
+let grid = {
+  color: "#000000bd",
+  size: 0.2,
+  state: true,
+};
 
-let colorGrid = "black"; //Cor do grid
+const drawGrid = () => {
+  //Tamanho das linhas e cor do grid
+  ctx.lineWidth = grid.size;
+  ctx.strokeStyle = grid.color;
+
+  if (grid.state) {
+    for (let i = size; i < canvasSize.x; i += size) {
+      //Desenha linhas na vertical
+      ctx.beginPath();
+      ctx.lineTo(i, 0);
+      ctx.lineTo(i, canvasSize.x);
+      ctx.stroke();
+    }
+
+    for (let i = size; i < canvasSize.x; i += size) {
+      //Desenha linhas na vertical
+      ctx.beginPath();
+      ctx.lineTo(0, i);
+      ctx.lineTo(canvasSize.x, i);
+      ctx.stroke();
+    }
+  }
+};
 
 /*-----Configurações da cobrinha-----*/
 
@@ -212,7 +186,7 @@ document.addEventListener("keydown", (event) => {
 
 /*-----Configurações da comida-----*/
 
-let colorFood = "red"; //Cor da comida
+let colorFood = "#cc1d1d"; //Cor da comida
 
 //Comida inicial
 const numRandom = (min, max) => {
@@ -261,6 +235,8 @@ const foodCollision = () => {
   if (foodCollision) {
     checkEat();
   }
+
+  playAudio();
 };
 
 const wallCollision = () => {
@@ -327,18 +303,13 @@ const checkEat = () => {
   scoreUpdate();
 };
 
-//Score
-const score = document.getElementById("score");
+let scoreAtual = parseInt(score.innerHTML, 10);
 
 //Atualiza o score
 const scoreUpdate = () => {
-  let scoreAtual = parseInt(score.innerHTML, 10);
-
   scoreAtual += 10;
 
   score.innerHTML = "" + scoreAtual;
-
-  console.log(scoreAtual);
 };
 
 //Termina o jogo
@@ -346,22 +317,16 @@ const gameOver = () => {
   moveON = false;
   direction = undefined;
 
+  score.innerHTML = "0";
+  finalScore.innerHTML = scoreAtual;
+
+  canvas.style.display = "none";
   gameoverScreen.style.display = "flex";
 };
 
 //Botão para reiniciar o jogo
-btnPlayAgain.forEach((e) => {
-  e.addEventListener("click", () => {
-    resetGame();
-  });
-});
-
-//Reset do jogo
-const resetGame = () => {
+btnPlayAgain.addEventListener("click", () => {
   clearInterval(gameInterval);
-
-  canvas.style.filter = "none";
-
   moveON = true;
 
   snake = [
@@ -369,13 +334,9 @@ const resetGame = () => {
     { x: 20, y: 10 },
   ];
 
-  food.x = numPositionX();
-  food.y = numPositionY();
-
-  startScreen.style.display = "flex";
-  gameScreen.style.display = "none";
   gameoverScreen.style.display = "none";
-};
+  startScreen.style.display = "flex";
+});
 
 /*-----Configurações para iniciar o jogo-----*/
 
@@ -392,6 +353,10 @@ let gameInterval;
 
 //Dá início ao jogo
 const playGame = () => {
+  if (window.innerHeight > window.innerWidth) {
+    Dpad.style.display = "grid";
+  }
+
   const food = {
     x: numPositionX(),
     y: numPositionY(),
@@ -412,32 +377,17 @@ const playGame = () => {
 //Botão para iniciar o jogo
 btnPlayGame.addEventListener("click", () => {
   startScreen.style.display = "none";
-  gameScreen.style.display = "flex";
+  canvas.style.display = "flex";
 
   playGame();
 });
 
 /*-----Configurações de jogo-----*/
 
-//Botão para pausar e despausar o jogo
-btnPauseGame.addEventListener("click", () => {
-  if (moveON) {
-    moveON = false;
-    pauseIcon.innerHTML = "play_arrow";
-  } else {
-    moveON = true;
-    pauseIcon.innerHTML = "pause";
-  }
-});
-
-//Botão para abriro menu de configurações
-btnOpenSettings.forEach((e) => {
-  e.addEventListener("click", () => {
-    moveON = false;
-
-    settingsScreen.style.display = "flex";
-    startScreen.style.display = "none";
-  });
+//Botão para abrir o menu de configurações
+btnOpenSettings.addEventListener("click", () => {
+  settingsScreen.style.display = "flex";
+  startScreen.style.display = "none";
 });
 
 btnCloseSettings.forEach((e) => {
@@ -449,46 +399,7 @@ btnCloseSettings.forEach((e) => {
   });
 });
 
-//Altera a velocidade da cobrinha
-buttonSpeed.forEach((e, i) => {
-  e.addEventListener("click", () => {
-    clearInterval(gameInterval);
-
-    snakeSpeed = parseInt(e.dataset.speed);
-
-    buttonSpeed.forEach((e) => {
-      e.style.backgroundColor = "#7ff26d";
-      e.style.color = "#1a2625";
-    });
-
-    e.style.backgroundColor = "#1d803b";
-  });
-});
-
-const drawGrid = () => {
-  //Tamanho das linhas e cor do grid
-  ctx.lineWidth = 0.1;
-  ctx.strokeStyle = colorGrid;
-
-  if (gridON) {
-    for (let i = size; i < canvasSize.x; i += size) {
-      //Desenha linhas na vertical
-      ctx.beginPath();
-      ctx.lineTo(i, 0);
-      ctx.lineTo(i, canvasSize.x);
-      ctx.stroke();
-    }
-
-    for (let i = size; i < canvasSize.x; i += size) {
-      //Desenha linhas na vertical
-      ctx.beginPath();
-      ctx.lineTo(0, i);
-      ctx.lineTo(canvasSize.x, i);
-      ctx.stroke();
-    }
-  }
-};
-
+//Altera a cor de fundo
 buttonBackgroundColor.forEach((e) => {
   e.addEventListener("click", () => {
     colorGame.color1 = e.dataset.color1;
@@ -497,13 +408,7 @@ buttonBackgroundColor.forEach((e) => {
   });
 });
 
-buttonGrid.addEventListener("click", () => {
-  gridON = !gridON; // Alterna o valor de gridON
-
-  gridIcon.innerHTML = gridON ? "toggle_on" : "toggle_off";
-  gridIcon.style.color = gridON ? "#7ff26d" : "#e3eed9";
-});
-
+//Altera a cor da cobrinha
 buttonSnakeColor.forEach((e) => {
   e.addEventListener("click", () => {
     colorSnake.colorHead = e.dataset.color1;
@@ -511,88 +416,61 @@ buttonSnakeColor.forEach((e) => {
   });
 });
 
-const gameSettings = () => {
-  //Botão para ativar e desativar o grid
-  const gridOnOff = document.getElementById("gridOnOff");
+//Altera a cor da comida
+buttonFoodColor.forEach((e) => {
+  e.addEventListener("click", () => {
+    colorFood = e.dataset.color;
+  });
+});
 
-  //Verifica se está ligado ou desligado
-  if (gridOnOff.checked) {
-    gridON = true;
-  } else {
-    gridON = false;
+buttonGrid.addEventListener("click", () => {
+  grid.state = !grid.state; // Alterna o valor de gridON
+
+  toggleButton[0].innerHTML = grid.state ? "toggle_on" : "toggle_off";
+  toggleButton[0].style.color = grid.state ? "#53d831" : "#747474";
+});
+
+buttonCollision.addEventListener("click", () => {
+  selfCollisionON = !selfCollisionON; // Alterna o valor de selfCollisionON
+
+  toggleButton[1].innerHTML = selfCollisionON ? "toggle_on" : "toggle_off";
+  toggleButton[1].style.color = selfCollisionON ? "#53d831" : "#747474";
+});
+
+// Altera a velocidade da cobrinha
+buttonSpeed.forEach((e, index) => {
+  // Define uma cor específica para o segundo botão ao iniciar
+  if (index === 1) {
+    e.style.backgroundColor = "#368a20"; // Cor inicial para o segundo botão
   }
 
-  const selfCollisionOnOff = document.getElementById("selfCollisionOnOff");
+  e.addEventListener("click", () => {
+    clearInterval(gameInterval);
+    snakeSpeed = parseInt(e.dataset.speed);
 
-  if (selfCollisionOnOff.checked) {
-    selfCollisionON = true;
-  } else {
-    selfCollisionON = false;
-  }
-
-  const btnSnakeColor = document.getElementsByClassName("btnSnakeColor");
-
-  for (let i = 0; i < btnSnakeColor.length; i++) {
-    btnSnakeColor[i].addEventListener("click", function () {
-      if (this === btnSnakeColor[0]) {
-        colorSnake.colorBody = "#d82b2b";
-        colorSnake.colorHead = "#ff0000";
-      } else if (this === btnSnakeColor[1]) {
-        colorSnake.colorBody = "#9c39c4";
-        colorSnake.colorHead = "#6c2b86";
-      } else if (this === btnSnakeColor[2]) {
-        colorSnake.colorBody = "#eeee5a";
-        colorSnake.colorHead = "#ffff00";
-      } else if (this === btnSnakeColor[3]) {
-        colorSnake.colorBody = "#4090f8";
-        colorSnake.colorHead = "#006eff";
-      } else if (this === btnSnakeColor[4]) {
-        colorSnake.colorBody = "#f8642e";
-        colorSnake.colorHead = "#ff4500";
-      } else if (this === btnSnakeColor[5]) {
-        colorSnake.colorBody = "#4dc942";
-        colorSnake.colorHead = "#1b9c0f";
-      }
+    // Reseta as cores de todos os botões
+    buttonSpeed.forEach((e) => {
+      e.style.backgroundColor = "#53d831";
     });
-  }
 
-  const btnFoodColor = document.getElementsByClassName("btnFoodColor");
+    // Destaca o botão clicado
+    e.style.backgroundColor = "#368a20";
+  });
+});
 
-  for (let i = 0; i < btnFoodColor.length; i++) {
-    btnFoodColor[i].addEventListener("click", function () {
-      if (this === btnFoodColor[0]) {
-        colorFood = "#9c0000";
-      } else if (this === btnFoodColor[1]) {
-        colorFood = "#71009e";
-      } else if (this === btnFoodColor[2]) {
-        colorFood = "#afaf00";
-      } else if (this === btnFoodColor[3]) {
-        colorFood = "#00459e";
-      } else if (this === btnFoodColor[4]) {
-        colorFood = "#b12f00";
-      } else if (this === btnFoodColor[5]) {
-        colorFood = "#096800";
-      }
+/*-----Efeitos sonoros-----*/
+
+const playAudio = (audioFile, elementClass, event) => {
+  const buttons = document.querySelectorAll(`.${elementClass}`);
+
+  buttons.forEach((button) => {
+    button.addEventListener(event, () => {
+      const audio = new Audio(`../audio/${audioFile}.wav`);
+      audio.play();
     });
-  }
-
-  const btnControls = document.getElementsByClassName("btnControls");
-
-  for (let i = 0; i < btnControls.length; i++) {
-    btnControls[i].addEventListener("click", function () {
-      const Dpad = document.getElementById("Dpad").style;
-
-      if (this === btnControls[0]) {
-        Dpad.display = "grid";
-        Dpad.marginTop = "5px";
-        Dpad.gap = "5px";
-      } else if (this === btnControls[1]) {
-        Dpad.display = "flex";
-        Dpad.marginTop = "15px";
-        Dpad.gap = "10px";
-      } else if (this === btnControls[2]) {
-        Dpad.display = "none";
-      }
-    });
-  }
+  });
 };
+
+playAudio("sound-hover-button", "play-game", "mouseover");
+playAudio("sound-hover-button", "open-settings", "mouseover");
+playAudio("sound-hover-button", "play-again", "mouseover");
